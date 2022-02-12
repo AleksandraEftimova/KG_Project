4d948959542aa9b89120ff117ad4d169eeeb02c5
Aleksandra <aleftimova@gmail.com>
2022-02-13 00:32:11 +0100

    Commit

--- src/main.cpp
+++ src/main.cpp
@@ -80,7 +80,7 @@ int main() {
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     //floor vertices
-    float vertices1[] = {
+    float vertices[] = {
             //x      y      z       texture
             -5.0f, 0.0f, 5.0f,   0.0f, 0.0f,
             5.0f,  0.0f, 5.0f,   1.0f, 0.0f,
@@ -262,7 +262,7 @@ int main() {
     glBindVertexArray(VAO);
 
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices1), vertices1, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), nullptr);
